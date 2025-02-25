11c18253e757df90139cddbcd6be12c1adb40cd0
Sean Quinn <Quinn-S101@ulster.ac.uk>
2025-02-25 15:38:49 +0000

    HEISNTDOUBLEJUMPING!!!!!
    WOOOOOOOOOOOOOOOOOOOOOO YAYYYYYYYYYYYYY!!!!!

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -10,48 +9,49 @@ public class PlayerMovement : MonoBehaviour
     private float Move;
 
     public float speed;
-
     public float jump;
 
     public LayerMask groundLayer;
 
     public Vector2 boxSize;
-
     public float castDistance;
 
-    // Start is called before the first frame update
+    private bool isGroundedFlag; 
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         Move = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(Move * speed, rb.velocity.y);
+        rb.velocity = new Vector2(Move * speed, rb.velocity.y); 
 
         
-        if (Input.GetButtonDown("Jump") && isGrounded())
+        if (Input.GetButtonDown("Jump") && isGroundedFlag)
         {
+            rb.velocity = new Vector2(rb.velocity.x, 0); 
             rb.AddForce(new Vector2(0, jump * 15), ForceMode2D.Impulse); 
         }
     }
 
-    
-    public bool isGrounded()
+    void FixedUpdate()
     {
         
-        bool grounded = Physics2D.BoxCast(transform.position, boxSize, 0, Vector2.down, castDistance, groundLayer);
-
-        return grounded;
+        CheckGrounded();
     }
 
-  
+    
+    private void CheckGrounded()
+    {
+        isGroundedFlag = Physics2D.BoxCast(transform.position, boxSize, 0, Vector2.down, castDistance, groundLayer);
+    }
+
+    
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.red;  
+        Gizmos.color = Color.red;
         Gizmos.DrawWireCube(transform.position - Vector3.up * (castDistance / 2), new Vector3(boxSize.x, boxSize.y, 0));
     }
 }
-
