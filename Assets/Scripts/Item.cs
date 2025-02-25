beb44c19d21b7f62f2b2f7c231628acfd2f38b15
Reannan Meggitt <Meggitt-R@ulster.ac.uk>
2025-02-25 14:07:18 +0000

    still working on inventory

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    [SerializeField]
+    private string itemName;
+
+    [SerializeField]
+    private int quantity;
+
+    [SerializeField]
+    private Sprite Sprite;
+
+    private InventoryManager inventoryManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      inventoryManager = GameObject.Find("InventoryCanvas")?.GetComponent<InventoryManager>();
+        if (inventoryManager == null)
+        {
+            Debug.LogError("InventoryManager not found on InventoryCanvas!");
+        }
+       
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            inventoryManager.AddItem(itemName, quantity, Sprite);            
+            Destroy(gameObject);
+        }
+    }
+}
