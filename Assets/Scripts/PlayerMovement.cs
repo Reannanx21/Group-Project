81dffabda3a99223cb2f6584aee841dc32dbf094
Sean Quinn <Quinn-S101@ulster.ac.uk>
2025-02-25 12:55:41 +0000

    UpdatedWithWorkingJump
    Grounded now works :3

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,6 +12,13 @@ public class PlayerMovement : MonoBehaviour
     public float speed;
 
     public float jump;
+
+    public LayerMask groundLayer;
+
+    public Vector2 boxSize;
+
+    public float castDistance;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,9 +30,28 @@ public class PlayerMovement : MonoBehaviour
     {
         Move = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(Move * speed, rb.velocity.y);
-        if (Input.GetButtonDown("Jump"))
+
+        
+        if (Input.GetButtonDown("Jump") && isGrounded())
         {
-            rb.AddForce(new Vector2(rb.velocity.x, jump * 10));
+            rb.AddForce(new Vector2(0, jump * 15), ForceMode2D.Impulse); 
         }
     }
+
+    
+    public bool isGrounded()
+    {
+        
+        bool grounded = Physics2D.BoxCast(transform.position, boxSize, 0, Vector2.down, castDistance, groundLayer);
+
+        return grounded;
+    }
+
+  
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;  
+        Gizmos.DrawWireCube(transform.position - Vector3.up * (castDistance / 2), new Vector3(boxSize.x, boxSize.y, 0));
+    }
 }
+
