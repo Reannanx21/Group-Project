e0247f64e815ccc11a8b50e688ed008f7284649b
Sean Quinn <Quinn-S101@ulster.ac.uk>
2025-03-03 14:31:37 +0000

    Flying Square is no more

--- Assets/aichase.cs
+++ Assets/aichase.cs
@@ -8,6 +8,10 @@ public class AIChase : MonoBehaviour
     public float speed = 5f;
     private float distance;
     private Rigidbody2D rb;
+    public LayerMask groundLayer;
+    public Vector2 boxSize;
+    public float castDistance;
+    public float groundCheckDistance = 1f; 
 
     void Start()
     {
@@ -17,21 +21,39 @@ public class AIChase : MonoBehaviour
             player = GameObject.FindGameObjectWithTag("Player");
             if (player == null)
             {
-                return; 
+                return;
             }
         }
     }
 
     void Update()
     {
-        if (player == null) return; 
+        if (player == null) return;
 
         distance = Vector2.Distance(transform.position, player.transform.position);
 
         if (distance < 20)
         {
             Vector2 direction = (player.transform.position - transform.position).normalized;
-            rb.MovePosition(rb.position + direction * speed * Time.deltaTime);
+
+            
+            RaycastHit2D groundCheck = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance);
+            if (groundCheck.collider != null)
+            {
+                
+                rb.MovePosition(new Vector2(rb.position.x + direction.x * speed * Time.deltaTime, rb.position.y + direction.y * speed * Time.deltaTime));
+            }
+            else
+            {
+                
+                rb.MovePosition(new Vector2(rb.position.x + direction.x * speed * Time.deltaTime, rb.position.y));
+            }
         }
+
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(transform.position - Vector3.up * (castDistance / 2), new Vector3(boxSize.x, boxSize.y, 0));
     }
 }
