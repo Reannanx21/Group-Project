6d8174ced358db360a5c6197536620715ab50daa
Sean Quinn <Quinn-S101@ulster.ac.uk>
2025-03-03 12:43:51 +0000

    Sean Movement

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -12,20 +11,46 @@ public class PlayerMovement : MonoBehaviour
     public float speed;
 
     public float jump;
+
+    public LayerMask groundLayer;
+
+    public Vector2 boxSize;
+    public float castDistance;
+
+    private bool isGroundedFlag;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         Move = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(Move * speed, rb.velocity.y);
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && isGroundedFlag)
         {
-            rb.AddForce(new Vector2(rb.velocity.x, jump * 10));
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+            rb.AddForce(new Vector2(0, jump * 15), ForceMode2D.Impulse);
         }
+    }
+
+    void FixedUpdate()
+    {
+        CheckGrounded();
+    }
+
+
+    private void CheckGrounded()
+    {
+        isGroundedFlag = Physics2D.BoxCast(transform.position, boxSize, 0, Vector2.down, castDistance, groundLayer);
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position - Vector3.up * (castDistance / 2), new Vector3(boxSize.x, boxSize.y, 0));
     }
 }
