2898a430d9a7baff1b099ed2f6b78847e02efcd4
Sean Quinn <Quinn-S101@ulster.ac.uk>
2025-03-10 12:29:06 +0000

    Animations working

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -1,31 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
-
+    private Animator animator;
     private float Move;
 
     public float speed;
-
     public float jump;
-    // Start is called before the first frame update
+
+    public LayerMask groundLayer;
+
+    public Vector2 boxSize;
+    public float castDistance;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         Move = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(Move * speed, rb.velocity.y);
+
+        if (animator != null)
+        {
+            animator.SetBool("HenryWalk", Mathf.Abs(Move) > 0.01f);
+        }
+
         if (Input.GetButtonDown("Jump"))
         {
-            rb.AddForce(new Vector2(rb.velocity.x, jump * 10));
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+            rb.AddForce(new Vector2(0, jump * 15), ForceMode2D.Impulse);
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position - Vector3.up * (castDistance / 2), new Vector3(boxSize.x, boxSize.y, 0));
     }
 }
