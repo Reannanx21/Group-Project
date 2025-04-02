2719cf3bbf10abeff3a2a36992872eec10eef281
Reannan Meggitt <Meggitt-R@ulster.ac.uk>
2025-04-02 12:50:17 +0100

    More sound effects

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -3,32 +3,43 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
     [Header("---------- Audio Sorce ----------")]
-    [SerializeField] AudioSource musicSource; // For background music
-    [SerializeField] AudioSource SFXSource1;  // For sound effects
-    [SerializeField] AudioSource SFXSource2;  // Another for overlapping SFX
-
+    [SerializeField] AudioSource musicSource; 
+    [SerializeField] AudioSource SFXSource1;  
+    [SerializeField] AudioSource SFXSource2;
+    [SerializeField] AudioSource SFXSource3;
+    [SerializeField] AudioSource SFXSource4;
     [Header("---------- Audio Clip ----------")]
     public AudioClip MainMenu;
-    public AudioClip Death;
+    public AudioClip RowBoat;
     public AudioClip Walking;
     public AudioClip Cannon;
     public AudioClip Waves;
+    public AudioClip Bell;
+
 
     private void Start()
     {
-        // Play main menu music
+        
         musicSource.clip = MainMenu;
-        musicSource.loop = true;  // Ensure it loops
+        musicSource.loop = true; 
         musicSource.Play();
 
-        // Play the waves sound effect
+       
         SFXSource1.clip = Waves;
-        SFXSource1.loop = true;  // Ensure it loops
+        SFXSource1.loop = true;  
         SFXSource1.Play();
 
-        // Play the cannon sound effect
+      
         SFXSource2.clip = Cannon;
-        SFXSource2.loop = true;  // Ensure it loops
+        SFXSource2.loop = true;  
         SFXSource2.Play();
+
+        SFXSource3.clip = RowBoat;
+       
+        SFXSource3.Play();
+
+        SFXSource4.clip = Bell;
+
+        SFXSource4.Play();
     }
 }
