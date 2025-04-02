3d28773fcc3463fc1691fbedfc87af94151bf41c
Reannan Meggitt <Meggitt-R@ulster.ac.uk>
2025-04-02 13:20:10 +0100

    controls music added

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
     [Header("---------- Audio Sorce ----------")]
-    [SerializeField] AudioSource musicSource; 
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource musicSource2;
     [SerializeField] AudioSource SFXSource1;  
     [SerializeField] AudioSource SFXSource2;
     [SerializeField] AudioSource SFXSource3;
     [SerializeField] AudioSource SFXSource4;
+   
     [Header("---------- Audio Clip ----------")]
     public AudioClip MainMenu;
     public AudioClip RowBoat;
@@ -15,16 +17,24 @@ public class AudioManager : MonoBehaviour
     public AudioClip Cannon;
     public AudioClip Waves;
     public AudioClip Bell;
+    public AudioClip Controls;
 
 
     private void Start()
     {
         
+       
+        
         musicSource.clip = MainMenu;
         musicSource.loop = true; 
         musicSource.Play();
 
-       
+        musicSource2.clip = Controls;
+        musicSource2.loop = true;
+        musicSource2.Play();
+
+
+
         SFXSource1.clip = Waves;
         SFXSource1.loop = true;  
         SFXSource1.Play();
