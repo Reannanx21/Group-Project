06e47f0fc1207dc6bfccaba28cab2fe1f014e2db
Sean Quinn <Quinn-S101@ulster.ac.uk>
2025-04-08 12:15:58 +0100

    testing git

--- Assets/PlayerAttack.cs
+++ Assets/PlayerAttack.cs
@@ -7,7 +7,7 @@ public class PlayerAttack : MonoBehaviour
     public GameObject attackPoint;
     public float radius = 0.5f;
     public LayerMask enemies;
-    public LayerMask destructibles; 
+    public LayerMask destructibles;
     public float damage = 10f;
     public bool isAttacking = false;
 
@@ -31,7 +31,7 @@ public class PlayerAttack : MonoBehaviour
     {
         isAttacking = true;
 
-        anim.SetTrigger("Attack");
+        anim.SetBool("IsAttacking", true);
 
         bool anythingHit = false;
 
@@ -60,7 +60,6 @@ public class PlayerAttack : MonoBehaviour
             Collider2D[] destructibleHits = Physics2D.OverlapCircleAll(attackPoint.transform.position, radius, destructibles);
             foreach (Collider2D obj in destructibleHits)
             {
-                // If tagged "Destructible", destroy it
                 if (obj.CompareTag("Destructible"))
                 {
                     Destroy(obj.gameObject);
@@ -68,7 +67,6 @@ public class PlayerAttack : MonoBehaviour
                     anythingHit = true;
                 }
 
-                // If it has ObjectHealth, damage it
                 ObjectHealth health = obj.GetComponent<ObjectHealth>();
                 if (health != null)
                 {
@@ -84,9 +82,11 @@ public class PlayerAttack : MonoBehaviour
         }
     }
 
+    // Called from Animation Event at end of attack animation
     public void ResetAttack()
     {
         isAttacking = false;
+        anim.SetBool("IsAttacking", false);
     }
 
     private void OnDrawGizmos()
